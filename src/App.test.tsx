ce2f584b552bef77b6917591d26d262598411870
tvelic1 <tvelic1@etf.unsa.ba>
2024-05-10 16:11:45 +0200

    added tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import Details from './components/Details';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./fetchData/api', () => ({
+  fetchFindById: jest.fn().mockResolvedValue(null), 
+  fetchSearchVideo: jest.fn().mockResolvedValue([])
+}));
+
+describe('Details Component Movie not found', () => {
+  test('displays a not found message when no movie details are available', async () => {
+    render(
+      <BrowserRouter>
+        <Details id="2786455454545" type="movie" />
+      </BrowserRouter>
+    );
+
+    const notFoundMessage = await screen.findByText(/There is no movie with this ID/i);
+    expect(notFoundMessage).toBeInTheDocument();
+  });
 });
+
+
+describe('Details Component TV show not found', () => {
+    test('displays a not found message when no TV show details are available', async () => {
+      render(
+        <BrowserRouter>
+          <Details id="2786455454545" type="tv" />
+        </BrowserRouter>
+      );
+  
+      const notFoundMessage = await screen.findByText(/There is no TV Show with this ID/i);
+      expect(notFoundMessage).toBeInTheDocument();
+    });
+  });
+  
+
