8bb2871785f0c1863bbd5c9ed5f21f5ff4752ac1
tvelic1 <92871186+tvelic1@users.noreply.github.com>
2024-06-29 16:42:50 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <i className="fas fa-film" style={{ color: "white" }} />
-          <p id="nav-title">Rubicon</p>
+          <p id="nav-title">Cinestar</p>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
